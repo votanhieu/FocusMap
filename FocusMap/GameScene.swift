a38cceeecc7c251445c2ddd1a37e973540ef8ee2
hieuvo <votanhieu.7@gmail.com>
2025-12-05 23:13:46 +0700

    no message

---
+++ FocusMap/GameScene.swift
@@ -0,0 +1,21 @@
+import SpriteKit
+
+class GameScene: SKScene {
+    override func didMove(to view: SKView) {
+    }
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    }
+
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+    }
+
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+    }
+
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+    }
+
+    override func update(_ currentTime: TimeInterval) {
+    }
+}
